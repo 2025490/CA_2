62b0db0acc9f2e688f6cca2ac0b5f8ede93a332f
2025490 <2025490@student.cct.ie>
2026-05-16 14:09:14 +0100

    3 - Manager class implemented

--- CA_2/src/main/java/CA_2/Manager.java
+++ CA_2/src/main/java/CA_2/Manager.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package CA_2;
 
-/**
- *
- * @author mathe
- */
-public class Manager {
-    
+// Manager extends Employee, meaning it inherits all the attributes and methods from Employee
+public class Manager extends Employee {
+
+    // Manager has an extra attribute - their level of authority in the bank
+    private String managerLevel;
+
+    // Constructor - we pass all employee details plus the manager level
+    public Manager(String name, String managerType, String department, String managerLevel) {
+        // This calls the Employee constructor to set name, managerType and department
+        super(name, managerType, department);
+        this.managerLevel = managerLevel;
+    }
+
+    // Getter for manager level
+    public String getManagerLevel() {
+        return managerLevel;
+    }
+
+    // Setter for manager level
+    public void setManagerLevel(String managerLevel) {
+        this.managerLevel = managerLevel;
+    }
+
+    // Overrides the Employee toString to also show the manager level
+    @Override
+    public String toString() {
+        return super.toString() + " | Manager Level: " + managerLevel;
+    }
 }
