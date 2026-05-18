fe7ee248d5c32309a34cde907f2a6ad97c94ac3a
2025490 <2025490@student.cct.ie>
2026-05-18 13:44:58 +0100

    4 - Department class implemented

--- CA_2/src/main/java/CA_2/Department.java
+++ CA_2/src/main/java/CA_2/Department.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package CA_2;
 
-/**
- *
- * @author mathe
- */
 public class Department {
-    
+
+    // Department has a name and a department code
+    private String departmentName;
+    private String departmentCode;
+
+    // Constructor to create a new department
+    public Department(String departmentName, String departmentCode) {
+        this.departmentName = departmentName;
+        this.departmentCode = departmentCode;
+    }
+
+    // Getter for department name
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    // Getter for department code
+    public String getDepartmentCode() {
+        return departmentCode;
+    }
+
+    // Setter for department name
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    // Setter for department code
+    public void setDepartmentCode(String departmentCode) {
+        this.departmentCode = departmentCode;
+    }
+
+    // Prints department info in a readable format
+    @Override
+    public String toString() {
+        return "Department: " + departmentName + " | Code: " + departmentCode;
+    }
 }
